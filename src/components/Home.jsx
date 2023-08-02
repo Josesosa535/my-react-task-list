d15a9392bea24b7660387eeca4d35311252fda08
Jose Sosa <josesosa535@gmail.com>
2023-08-02 16:19:20 -0500

    se agregaron las rutas

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import { FaTasks } from "react-icons/fa";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="tituloH">Bienvenido a la Aplicación de Tareas</h1>
+      <FaTasks className="iconoHome" />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/SobreNosotros.jsx
@@ -0,0 +1,57 @@
+const SobreNosotros = () => {
+  return (
+    <div>
+      <h1 className="tituloSN">Sobre Nosotros</h1>
+      <p>
+        La aplicación de "Lista de Tareas" es una poderosa herramienta diseñada
+        para ayudar a los usuarios a gestionar y organizar sus actividades
+        diarias de manera eficiente. Esta aplicación permite a los usuarios
+        registrar, editar y eliminar tareas que necesitan realizar en diferentes
+        momentos, lo que les brinda una visión clara y estructurada de sus
+        responsabilidades y metas. Funcionalidades clave: 1. Añadir Tareas: Los
+        usuarios pueden agregar fácilmente nuevas tareas a la aplicación,
+        proporcionando detalles como el título de la tarea, la descripción, la
+        fecha límite y cualquier otra información relevante. Esto les permite
+        mantener un registro actualizado de sus obligaciones pendientes. 2.
+        Editar Tareas: La aplicación permite a los usuarios realizar cambios en
+        las tareas existentes. Pueden actualizar la descripción, modificar la
+        fecha límite, o realizar ajustes necesarios para adaptarse a cualquier
+        cambio en sus planes o prioridades. 3. Eliminar Tareas: Los usuarios
+        pueden eliminar tareas que ya han sido completadas o que ya no son
+        relevantes. Esta función les ayuda a mantener una lista de tareas
+        activas más concisa y enfocada. 4. Visualización de Tareas: La
+        aplicación proporciona una interfaz de usuario intuitiva para que los
+        usuarios puedan ver todas sus tareas registradas. Las tareas se
+        presentan en una lista organizada, lo que facilita el acceso a la
+        información importante en un solo vistazo. 5. Recordatorios y
+        Notificaciones: La aplicación puede estar equipada con funciones de
+        recordatorio o notificación, alertando a los usuarios sobre próximas
+        fechas límite o tareas pendientes. Esto ayuda a evitar olvidos y asegura
+        que se cumplan los plazos importantes. 6. Facilidad de Uso: La
+        aplicación está diseñada de manera intuitiva y amigable para el usuario.
+        Cualquier persona, independientemente de su experiencia tecnológica,
+        puede utilizarla sin dificultades. Beneficios de la Aplicación: -
+        Gestión Eficaz del Tiempo: Al tener un registro claro de las tareas
+        pendientes y las fechas límite, los usuarios pueden priorizar sus
+        actividades y administrar su tiempo de manera más eficiente. -
+        Organización y Productividad: La aplicación ayuda a los usuarios a
+        mantenerse organizados, reducir el estrés y aumentar su productividad al
+        asegurar que no se olvide ninguna tarea importante. - Colaboración y
+        Compartir Tareas: En algunas versiones avanzadas, la aplicación puede
+        permitir a los usuarios compartir tareas con otros miembros del equipo o
+        familiares, facilitando la colaboración y el trabajo en equipo. - Acceso
+        desde cualquier lugar: Si la aplicación se encuentra alojada en la nube,
+        los usuarios pueden acceder a sus tareas desde cualquier dispositivo con
+        conexión a Internet, lo que les brinda flexibilidad y movilidad. En
+        resumen, la aplicación de "Añadir, Editar y Eliminar Tareas" es una
+        valiosa solución para cualquier persona que desee mejorar su
+        organización personal y aumentar su productividad. Proporciona una forma
+        sencilla y efectiva de mantener un seguimiento de las tareas
+        importantes, asegurando que los usuarios puedan cumplir con sus
+        objetivos y responsabilidades en el tiempo adecuado.
+      </p>
+    </div>
+  );
+};
+
+export default SobreNosotros;
